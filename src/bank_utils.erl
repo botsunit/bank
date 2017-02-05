d758f105efaf788a9c5409b8f3511a62faeaf138
glejeune <gregoire.lejeune@free.fr>
2017-02-05 21:01:56 +0100

    Update rebar3_elixir for Elixir 1.4
    Use bucrandom to generate fake BIC and IBAN

--- src/bank_utils.erl
+++ src/bank_utils.erl
@@ -13,9 +13,5 @@ fake_alphanum(Size) ->
   bucs:to_binary(fake(Size, "AZERTYUIOPQSDFGHJKLMWXCVBN1234567890")).
 
 fake(Length, AllowedChars) ->
-  bucs:to_binary(
-    lists:foldl(fun(_, Acc) ->
-                    [lists:nth(random:uniform(length(AllowedChars)),
-                               AllowedChars)]
-                    ++ Acc
-                end, [], lists:seq(1, Length))).
+  bucs:to_binary(bucrandom:randstr(Length, AllowedChars)).
+
